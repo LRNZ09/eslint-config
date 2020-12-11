5e684f99ecd2f192cb66625e511f60a9c3a31c20
Lorenzo Pieri <email@lorenzopieri.dev>
2020-12-11 11:19:31 +0100

    feat: add emotion lint config

---
+++ emotion.js
@@ -0,0 +1,10 @@
+module.exports = {
+	plugins: ['emotion'],
+	rules: {
+		'@emotion/import-from-emotion': 'error',
+		'@emotion/no-vanilla': 'error',
+		'@emotion/syntax-preference': ['error', 'object'],
+		'@emotion/styled-import': 'error',
+		'@emotion/jsx-import': 'off',
+	},
+}
